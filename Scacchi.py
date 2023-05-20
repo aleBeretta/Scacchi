1646ee562537f6f5d6e29426bc8818a1a20076e4
aleBeretta <beretta.alessandro06@gmail.com>
2023-05-20 17:37:34 +0200

    Inizio a programmare come fare le mosse

--- Scacchi.py
+++ Scacchi.py
@@ -1,133 +1,163 @@
-import pygame, sys
+import pygame
+import sys
 from pygame.locals import *
 pygame.init()
-WINDOW_SIZE=(1200,680)
-screen=pygame.display.set_mode(WINDOW_SIZE)
-screen.fill((240,240,180))
+WINDOW_SIZE = (1200, 680)
+screen = pygame.display.set_mode(WINDOW_SIZE)
+screen.fill((240, 240, 180))
 pygame.display.set_caption("Scacchi")
 
 
-clock=pygame.time.Clock()
-fps=60
+clock = pygame.time.Clock()
+fps = 60
 
-print("prova")      
 
 class Square:
     def __init__(self, x, y, larg, alt, pezzo=None) -> None:
-        self.larg=larg
-        self.alt=alt
+        self.larg = larg
+        self.alt = alt
 
-        self.x=x
-        self.xtot=x*larg+344
-        
-        self.y=y
-        self.ytot=y*alt+84
+        self.x = x
+        self.xtot = x*larg+344
 
-        self.coord=(self.xtot,self.ytot)
+        self.y = y
+        self.ytot = y*alt+84
 
-        self.color="light" if (x+y)%2==0 else "dark"
-        self.norm_color=(245, 237, 198) if self.color=="light" else(75,37,17)
-        
-        self.rect=pygame.Rect(self.xtot, self.ytot, self.larg, self.alt)
+        self.coord = (self.xtot, self.ytot)
 
+        self.color = "light" if (x+y) % 2 == 0 else "dark"
+        self.norm_color = (245, 237, 198) if self.color == "light" else (75, 37, 17)
+
+        self.rect = pygame.Rect(self.xtot, self.ytot, self.larg, self.alt)
+        self.pezzo = pezzo
 
     def draw(self):
-        pygame.draw.rect(screen, self.norm_color, self.rect)            
+        pygame.draw.rect(screen, self.norm_color, self.rect)
 
+    def drawcircle(self):
+        colore = (0, 65, 10)
+        raggio = 15
+        circleimage = pygame.Surface((raggio*2, raggio*2))
+        circlerect = circleimage.get_rect()
+        circlerect.center = self.rect.center
+        print(circlerect.center)
+
+        pygame.draw.circle(circleimage, colore, circlerect.center, raggio)
+        screen.blit(circleimage, circlerect)
 
 
 class Pawn:
     def __init__(self, colore, coord) -> None:
-        self.colore=colore
-        self.coord=coord
-        self.pos=(self.coord[1]*64+344, self.coord[0]*64+84)
-        self.image=pygame.image.load("pb.png").convert_alpha() if self.colore=="B" else pygame.image.load("pn.png").convert_alpha()
-        self.rect=self.image.get_rect(topleft=self.pos)
-        self.rect.width=64
-        self.rect.height=64
-        self.image=pygame.transform.scale(self.image,(64,64))
-    
+        self.colore = colore
+        self.coord = coord
+        self.pos = (self.coord[1]*64+344, self.coord[0]*64+84)
+        self.image = pygame.image.load("pb.png").convert_alpha() if self.colore == "B" else pygame.image.load("pn.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft=self.pos)
+        self.rect.width = 64
+        self.rect.height = 64
+        self.image = pygame.transform.scale(self.image, (64, 64))
+
     def draw(self):
         screen.blit(self.image, self.rect)
 
-    
+    def showmoves(self):
+        possmoves = []
+        if self.colore == "N":
+            if Scacchiera.board[self.coord[0]+1][self.coord[1]].pezzo == None:
+                possmoves.append(Scacchiera.board[self.coord[0]+1][self.coord[1]])
+        else:
+            if Scacchiera.board[self.coord[0]-1][self.coord[1]].pezzo == None:
+                possmoves.append(Scacchiera.board[self.coord[0]-1][self.coord[1]])
+        
+        for move in possmoves:
+            move.drawcircle()
+        return possmoves
+
 
 class King:
     def __init__(self, colore, coord) -> None:
-        self.colore=colore
-        self.coord=coord
-        self.pos=(self.coord[1]*64+344, self.coord[0]*64+84)
-        self.image=pygame.image.load("reb.png").convert_alpha() if self.colore=="B" else pygame.image.load("ren.png").convert_alpha()
-        self.rect=self.image.get_rect(topleft=self.pos)
-        self.rect.width=64
-        self.rect.height=64
-        self.image=pygame.transform.scale(self.image,(64,64))
-    
+        self.colore = colore
+        self.coord = coord
+        self.pos = (self.coord[1]*64+344, self.coord[0]*64+84)
+        self.image = pygame.image.load("reb.png").convert_alpha(
+        ) if self.colore == "B" else pygame.image.load("ren.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft=self.pos)
+        self.rect.width = 64
+        self.rect.height = 64
+        self.image = pygame.transform.scale(self.image, (64, 64))
+
     def draw(self):
         screen.blit(self.image, self.rect)
+
 
 class Queen:
     def __init__(self, colore, coord) -> None:
-        self.colore=colore
-        self.coord=coord
-        self.pos=(self.coord[1]*64+344, self.coord[0]*64+84)
-        self.image=pygame.image.load("regb.png").convert_alpha() if self.colore=="B" else pygame.image.load("regn.png").convert_alpha()
-        self.rect=self.image.get_rect(topleft=self.pos)
-        self.rect.width=64
-        self.rect.height=64
-        self.image=pygame.transform.scale(self.image,(64,64))
-    
+        self.colore = colore
+        self.coord = coord
+        self.pos = (self.coord[1]*64+344, self.coord[0]*64+84)
+        self.image = pygame.image.load("regb.png").convert_alpha(
+        ) if self.colore == "B" else pygame.image.load("regn.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft=self.pos)
+        self.rect.width = 64
+        self.rect.height = 64
+        self.image = pygame.transform.scale(self.image, (64, 64))
+
     def draw(self):
         screen.blit(self.image, self.rect)
+
 
 class Bishop:
     def __init__(self, colore, coord) -> None:
-        self.colore=colore
-        self.coord=coord
-        self.pos=(self.coord[1]*64+344, self.coord[0]*64+84)
-        self.image=pygame.image.load("ab.png").convert_alpha() if self.colore=="B" else pygame.image.load("an.png").convert_alpha()
-        self.rect=self.image.get_rect(topleft=self.pos)
-        self.rect.width=64
-        self.rect.height=64
-        self.image=pygame.transform.scale(self.image,(64,64))
-    
+        self.colore = colore
+        self.coord = coord
+        self.pos = (self.coord[1]*64+344, self.coord[0]*64+84)
+        self.image = pygame.image.load("ab.png").convert_alpha(
+        ) if self.colore == "B" else pygame.image.load("an.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft=self.pos)
+        self.rect.width = 64
+        self.rect.height = 64
+        self.image = pygame.transform.scale(self.image, (64, 64))
+
     def draw(self):
         screen.blit(self.image, self.rect)
+
 
 class Knight:
     def __init__(self, colore, coord) -> None:
-        self.colore=colore
-        self.coord=coord
-        self.pos=(self.coord[1]*64+344, self.coord[0]*64+84)
-        self.image=pygame.image.load("cb.png").convert_alpha() if self.colore=="B" else pygame.image.load("cn.png").convert_alpha()
-        self.rect=self.image.get_rect(topleft=self.pos)
-        self.rect.width=64
-        self.rect.height=64
-        self.image=pygame.transform.scale(self.image,(64,64))
-    
+        self.colore = colore
+        self.coord = coord
+        self.pos = (self.coord[1]*64+344, self.coord[0]*64+84)
+        self.image = pygame.image.load("cb.png").convert_alpha(
+        ) if self.colore == "B" else pygame.image.load("cn.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft=self.pos)
+        self.rect.width = 64
+        self.rect.height = 64
+        self.image = pygame.transform.scale(self.image, (64, 64))
+
     def draw(self):
         screen.blit(self.image, self.rect)
+
 
 class Rook:
     def __init__(self, colore, coord) -> None:
-        self.colore=colore
-        self.coord=coord
-        self.pos=(self.coord[1]*64+344, self.coord[0]*64+84)
-        self.image=pygame.image.load("tb.png").convert_alpha() if self.colore=="B" else pygame.image.load("tn.png").convert_alpha()
-        self.rect=self.image.get_rect(topleft=self.pos)
-        self.rect.width=64
-        self.rect.height=64
-        self.image=pygame.transform.scale(self.image,(64,64))
-    
+        self.colore = colore
+        self.coord = coord
+        self.pos = (self.coord[1]*64+344, self.coord[0]*64+84)
+        self.image = pygame.image.load("tb.png").convert_alpha() if self.colore == "B" else pygame.image.load("tn.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft=self.pos)
+        self.rect.width = 64
+        self.rect.height = 64
+        self.image = pygame.transform.scale(self.image, (64, 64))
+
     def draw(self):
         screen.blit(self.image, self.rect)
-
 
 
 class Board:
     def __init__(self) -> None:
-        self.board=[[Square(c, r, 64, 64) for c in range(8)] for r in range(8)]
-        self.pezzi=[]
+        self.board = [[Square(c, r, 64, 64) for c in range(8)]
+                      for r in range(8)]
+        self.pezzi = []
 
     def draw(self):
         for r in range(8):
@@ -137,7 +167,8 @@ class Board:
         for pezzo in self.pezzi:
             pezzo.draw()
 
-Scacchiera=Board()
+
+Scacchiera = Board()
 # Scacchiera.board[0][0].pezzo, Scacchiera.board[0][7].pezzo=Rook("N", Scacchiera.board[0][0].coord), Rook("N", Scacchiera.board[0][7].coord)
 # Scacchiera.board[7][0].pezzo, Scacchiera.board[7][7].pezzo=Rook("B", Scacchiera.board[7][0].coord), Rook("B", Scacchiera.board[7][7].coord)
 # Scacchiera.board[0][1].pezzo, Scacchiera.board[0][6].pezzo=Knight("N", Scacchiera.board[0][1].coord), Knight("N", Scacchiera.board[0][6].coord)
@@ -155,8 +186,8 @@ Scacchiera=Board()
 #     Scacchiera.pezzi.append(Scacchiera.board[7][i].pezzo)
 
 for i in range(8):
-    Scacchiera.pezzi.append(Pawn("N", (1,i)))
-    Scacchiera.pezzi.append(Pawn("B", (6,i)))
+    Scacchiera.pezzi.append(Pawn("N", (1, i)))
+    Scacchiera.pezzi.append(Pawn("B", (6, i)))
 Scacchiera.pezzi.append(Rook("N", (0, 0)))
 Scacchiera.pezzi.append(Rook("N", (0, 7)))
 Scacchiera.pezzi.append(Rook("B", (7, 0)))
@@ -174,26 +205,24 @@ Scacchiera.pezzi.append(King("B", (7, 4)))
 Scacchiera.pezzi.append(Queen("N", (0, 3)))
 Scacchiera.pezzi.append(Queen("B", (7, 3)))
 
-
-
+for pezzo in Scacchiera.pezzi:
+    Scacchiera.board[pezzo.coord[0]][pezzo.coord[1]].pezzo = pezzo
 
 
 while True:
     for event in pygame.event.get():
-        if event.type==QUIT:
+        if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
-        if event.type==pygame.MOUSEBUTTONDOWN and event.button==1:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             pos = pygame.mouse.get_pos()
             for pezzo in Scacchiera.pezzi:
                 if pezzo.rect.collidepoint(pos):
-                   pass
+                    pezzo.showmoves()
+                    possmoves = pezzo.showmoves()
 
     Scacchiera.draw()
 
-
-    
-    
     pygame.display.update()
     clock.tick(fps)
